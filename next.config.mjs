17f1ec8e6a12e9a3c6680b61994a9235f99ae9f4
____AYE KYI PHYU____ <44504932+ayekyiphyu@users.noreply.github.com>
2025-05-20 15:50:15 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-output: 'export', // 👈 needed for static export
-  distDir: 'out',   // 👈 this is where `next export` puts files
-  basePath: '/book-web', // 👈 IMPORTANT for GitHub Pages
-  trailingSlash: true,   // 👈 ensures correct routing
+output: 'export', 
+  distDir: 'out',  
+  basePath: '/book-web', 
+  trailingSlash: true,   
 };
 
 export default nextConfig;
